2eb2d2356817795fc06404a78ac3dc2957a11607
Xinyu Li <lixinyu.arthur@outlook.com>
2016-11-12 11:54:03 -0500

    Merged Version

--- Q5.py
+++ Q5.py
@@ -1,17 +1,21 @@
+## PR Midterm 2016 fall
+## Xinyu Li (152000006)
+
 import numpy as np
 import math
 from numpy import matrix
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import networkx as nx
 
+
+# sampling fuction
 def uniformSample():
     return np.random.rand(20, 2)
+
+# calculate the probability
 def calProbInMatrix(sample):
     def calProb(x, y):
         def dist(a, b):
-            # print (a - b)
-            # print (a - b)**2
-            # print np.sum((a-b)**2)
             return np.sum((a - b) ** 2)
         return (1 / math.sqrt(2 * math.pi)) * math.exp((-4) * dist(x, y))
 
@@ -23,16 +27,17 @@ def calProbInMatrix(sample):
         for j in range(M):
             ProbV.append(calProb(X[i], X[j]))
         ProbMat.append(ProbV)
-    #print np.array(ProbMat).shape
     return ProbMat
+
+#
 def calAdMatrix(ProbMat):
-    #S = np.zeros((ProbMat.shape[0], ProbMat.shape[1]))
     adMat = (ProbMat > np.random.rand(len(ProbMat), len(ProbMat[1]))).astype(int)
     num_rows = len(adMat)
     for i in range(num_rows):
         for j in range(0, i):
             adMat[j][i] = adMat[i][j]
     return adMat
+
 #calculate inverse covariance matrix from adjacency matrix
 def calInverseCovMatrix(AdMat):
     invCovMat = []
@@ -50,12 +55,15 @@ def calInverseCovMatrix(AdMat):
             covList.append(cov)
         invCovMat.append(covList)
     return  invCovMat
+
 def inverseMatrix(invCovMat):
     CovMat = matrix(invCovMat).I
     return CovMat
+
 def convCoordinate(Vec):
     Vec = [ e for e in Vec for repetitions in range(len(Vec)) ]
     return Vec
+
 def drawGraph(X, Y, Z):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -72,6 +80,7 @@ def drawGraph(X, Y, Z):
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
     plt.show()
+
 def isSymmetrix(mat):
     return(mat.transpose() == mat).all()
 Sample = uniformSample()
@@ -80,58 +89,55 @@ AdMat = calAdMatrix(PrMat)
 invCovMat = calInverseCovMatrix(AdMat)
 CovMat = inverseMatrix(invCovMat)
 np.save("covariance_Matrix", CovMat)
-#print PrMat
-#print AdMat
-#print Sample
-def drawAdGraph(X, Admat):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(X[:,0], X[:,1],color = 'r')
-    for i in range(len(AdMat)):
-        for j in range(len(AdMat[0])):
-            if(AdMat[i][j] == 1):
-                plt.plot([X[i][0], X[j][0]], [X[i][1], X[j][1]])
 
+
+def drawAdGraph(X, Admat):
+    # fig = plt.figure()
+    G = nx.Graph()
+    rows, cols = np.where(AdMat == 1)
+    edges = zip(rows.tolist(), cols.tolist())
+    gr = nx.Graph()
+    gr.add_edges_from(edges)
+    nx.draw_networkx(gr)
+    plt.title('Adjacency Matrix')
     plt.show()
 
 def drawImage(mat):
-    #plt.imshow(np.asarray(mat), interpolation='nearest', cmap=plt.cm.ocean, extent=(0.5, 10.5, 0.5, 10.5))
+    plt.imshow(np.asarray(mat), interpolation='nearest', cmap=plt.cm.ocean, extent=(0.5, 10.5, 0.5, 10.5))
     plt.pcolor(np.asarray(mat))
     plt.colorbar()
     plt.show()
 
-drawImage(invCovMat)
-drawImage(CovMat)
+def drawAdGraph(AdMat, invCovMat,CovMat):
+
+    # graph
+    fig = plt.figure()
+    plt.subplot(131, aspect='equal')
+    rows, cols = np.where(AdMat == 1)
+    edges = zip(rows.tolist(), cols.tolist())
+    gr = nx.Graph()
+    gr.add_edges_from(edges)
+    nx.draw_networkx(gr)
+    plt.title('Adjacency Matrix')
+
+    # Cov
+    plt.subplot(132, aspect='equal')
+    im1=plt.imshow(np.asarray(CovMat), interpolation='nearest', cmap=plt.cm.RdBu_r)
+    plt.colorbar(im1, fraction=0.046, pad=0.04)
+    plt.title('Adjacency Matrix')
+
+    # Inverse
+    plt.subplot(133, aspect='equal')
+    im2 =plt.imshow(np.asarray(invCovMat), interpolation='nearest', cmap=plt.cm.RdBu_r)
+    plt.colorbar(im2, fraction=0.046, pad=0.04)
+    plt.title('Adjacency Matrix')
+
+    plt.show()
+
+
+
+
+
 print np.asarray(CovMat).shape, np.asarray(invCovMat).shape
 Z = [z for sublist in AdMat for z in sublist]
-# X = convCoordinate(Sample[:, 0])
-# Y = convCoordinate(Sample[:, 1])
-#print len(X), len(Y), len(Z)
-drawAdGraph(Sample, AdMat)
-#drawGraph(X, Y, Z)
-#print CovMat
-#print invCovMat * CovMat
-
-
-
-
-
-
-
-
-
-
-
-# #P = np.ones((4, 5)) * 0.2
-# P =[[ 0.4,  0.2,  0.2,  0.2 , 0.2]
-#  ,[ 0.2, 0.2 , 0.2 , 0.2 , 0.2]
-#  ,[ 0.2 , 0.2 , 0.2 , 0.2 , 0.2]
-#  ,[ 0.2 , 0.2  ,0.2 , 0.2  ,0.2]]
-# S = np.zeros((4, 5))
-# print P
-# print S
-# for i in range(10000):
-#     S += (P > np.random.rand(4, 5)).astype(int)
-#
-# print S          # each element should be approximately 20
-# print S.mean()   # the average  should be approximately 20, too
+drawAdGraph(AdMat, invCovMat,CovMat)
